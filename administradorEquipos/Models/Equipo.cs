27b03a0b0113b6f44ee3f197d755d462cf600818
Poul Lorca <plorca@donpollo.cl>
2022-09-30 17:25:53 -0300

    Agregar archivos de proyecto.

---
+++ administradorEquipos/Models/Equipo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace administradorEquipos.Models
+{
+    public class Equipo
+    {
+        public string NombreEquipo { get; set; } = null!;
+        public int CantidadJugadores { get; set; }
+        public string NombreDt { get; set; } = null!;
+        public string TipoEquipo { get; set; } = null!; 
+        public string CapitanEquipo { get; set; } = null!; 
+        public bool TieneSub21 { get; set; }
+
+        public Equipo()
+        {
+        }
+
+        public Equipo(string nombreEquipo, int cantidadJugadores, string nombreDt, string tipoEquipo, string capitanEquipo, bool tieneSub21)
+        {
+            NombreEquipo = nombreEquipo;
+            CantidadJugadores = cantidadJugadores;
+            NombreDt = nombreDt;
+            TipoEquipo = tipoEquipo;
+            CapitanEquipo = capitanEquipo;
+            TieneSub21 = tieneSub21;
+        }        
+
+
+    }
+}
