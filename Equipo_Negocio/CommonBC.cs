529f180983f3b62bce97dbc0390375f95de7da12
Poul Lorca <plorca@donpollo.cl>
2022-10-13 17:50:20 -0300

    Finished Negocio

---
+++ Equipo_Negocio/CommonBC.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Equipo_Negocio
+{
+    public class CommonBC
+    {
+        private static Equipo_DALC.PCEEntities _modeloEquipo;
+
+        public static Equipo_DALC.PCEEntities ModeloEquipo
+        {
+            get
+            {
+                if(_modeloEquipo == null)
+                {
+                    _modeloEquipo = new Equipo_DALC.PCEEntities();
+                }
+                return _modeloEquipo;
+            }
+        }
+
+        public CommonBC() { }
+    }
+}
---
+++ Equipo_Negocio/EquipoReportes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Equipo_Negocio
+{
+    public class EquipoReportes
+    {
+        public int ReporteEquiposFemeninos()
+        {
+            return Convert.ToInt32(CommonBC.ModeloEquipo.spObtenerCantidadEquiposFemeninos());
+        }
+
+        public int ReporteEquiposMasculinos()
+        {
+            return Convert.ToInt32(CommonBC.ModeloEquipo.spObtenerCantidadEquiposMasculinos());
+        }
+    }
+}
